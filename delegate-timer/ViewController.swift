3e79f46ab0537452fc335c8464b2ea0ae743c0ec
Yuta Shoji <shoshoshon.music@gmail.com>
2023-01-15 16:27:55 +0900

    Initial Commit

---
+++ delegate-timer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegate-timer
+//
+//  Created by Yuta Shoji on 2023/01/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
